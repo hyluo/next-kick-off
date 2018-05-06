c6cd9f6b5332947fec6bb99b8082d50cd05b61f3
hyluo <maplein42@gmail.com>
2018-05-06 16:20:24 +0800

    test

--- components/pages.js
+++ components/pages.js
@@ -1,25 +1,21 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const Menu = ({href, label}) => (
-    <span>
-    <Link href= {href} ><a>{label}</a></Link>
-    </span>
-)
+const Menu = ({ href, label }) => (
+  <span>
+    <Link href={href}>
+      <a>{label}</a>
+    </Link>
+  </span>
+);
 
-export default ({children}) => (
-    <main>
+export default ({ children }) => (
+  <main>
     <nav>
-        <Menu href={'/'} label= {'＾'} />
-        {
-            ['news', 'newest', 'ask', 'jobs'].map( m => (
-                <Menu 
-                key = {m}
-                href = {`/${m}`}
-                label = {m.toUpperCase()}
-                />
-            ))
-        }
+      <Menu href={'/'} label={'＾'} />
+      {['news', 'newest', 'ask', 'jobs'].map(m => (
+        <Menu key={m} href={`/${m}`} label={m.toUpperCase()} />
+      ))}
     </nav>
-    <div> {children} </div>    
-    </main>
-) 
+    <div> {children} </div>
+  </main>
+);
