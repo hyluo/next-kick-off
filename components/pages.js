0c03b42c7c52d394b4cd5d9530a762625a8cf33f
hyluo <maplein42@gmail.com>
2018-05-06 20:50:32 +0800

    basic menu css complete

--- components/pages.js
+++ components/pages.js
@@ -5,6 +5,18 @@ const Menu = ({ href, label }) => (
     <Link href={href}>
       <a>{label}</a>
     </Link>
+    <style jsx>
+      {`
+         {
+          padding: 6px 4px;
+        }
+        a {
+          color: #fff;
+          text-decoration: none;
+          font-size: 16px;
+        }
+      `}
+    </style>
   </span>
 );
 
@@ -26,6 +38,25 @@ export default ({ children }) => (
           text-align: left;
           background-color: #000;
           color: #fff;
+          padding: 12px;
+          position: fixed;
+          z-index: 1000;
+          top: 0;
+          left: 0;
+          right: 0;
+        }
+        a {
+          color: #0e0e0e;
+          text-decoration: none;
+        }
+      `}
+    </style>
+    <style jsx>
+      {`
+        div {
+          width: 100%;
+          color: #333;
+          padding-top: 40px;
         }
       `}
     </style>
