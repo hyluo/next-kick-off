0a3f8b49f2a8ec178575852ed443c8453aeb93ea
hyluo <maplein42@gmail.com>
2018-05-07 10:05:53 +0800

    fetch api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import { Component } from 'react';
 import Page from '../components/pages';
+import fetch from '../lib/fetch.js';
 
-export default () => (
-  <Page>
-    <div> welcome to next tutorial </div>
-  </Page>
-);
+export default class extends Component {
+  static async getInitialProps({ pathname, query }) {
+    const feed = pathname == '/' ? '/news' : pathname;
+
+    const page = query.page ? `/${query.page}` : '1';
+    return {
+      feeds: await fetch(`${feed}/${page}`)
+    };
+  }
+  render() {
+    return <Page>{this.props.feeds.map(f => <div>{f.title}</div>)}</Page>;
+  }
+}
